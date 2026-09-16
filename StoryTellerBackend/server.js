90cd390a0a4b269a1dfc753646959a8cb1f9aa93
Vivek Ranjan <ranjanvivek498@gmail.comm>
2026-09-16 15:27:35 +0530

    Security Middleware: Helmet , Cors, Request Body Limits

--- StoryTellerBackend/server.js
+++ StoryTellerBackend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const errorHandler = require("./middlewares/errorMiddleware");
+const helmet = require("helmet");
 
 dotenv.config();
 
@@ -11,8 +12,10 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "10kb" }));
 
 db.getConnection()
   .then((connection) => {
